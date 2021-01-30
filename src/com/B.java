918c119442ed9bdae87ed893404217c9488cc2ce
srav <waste2983@gmail.com>
2021-01-30 01:20:19 -0600

    B Class Added

---
+++ src/com/B.java
@@ -0,0 +1,19 @@
+package com.example.first;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+		System.out.println("***************");
+		System.out.prinln("omsairamji");
+		System.out.prinln("This is B class Changes");
+
+		
+		
+
+		
+
+
+	}
+
+}
