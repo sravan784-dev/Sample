2c3d205ea950e3595c3418cb3f28284b284a6c5d
srav <waste2983@gmail.com>
2021-01-30 01:04:43 -0600

    Fourth

--- src/com/example/first/A.java
+++ src/com/example/first/A.java
@@ -3,12 +3,17 @@ package com.example.first;
 public class A {
 
 	public static void main(String[] args) {
-		
+		System.out.println("***************");
+		System.out.prinln("omsairamji");
+		System.out.prinln("first commit");
+		System.out.prinln("second commit");
+		System.out.prinln("third commit");
+		System.out.println("***************");
+		System.out.prinln("omsairamji");
 		
 		
 
-		System.out.println("***************");
-		System.out.prinln("omsairamji");
+		
 
 
 	}
