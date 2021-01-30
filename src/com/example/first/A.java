d98eddb9bde06252c86cc28aae6ba02ba9947cda
srav <waste2983@gmail.com>
2021-01-30 01:02:43 -0600

    Second

--- src/com/example/first/A.java
+++ src/com/example/first/A.java
@@ -5,7 +5,6 @@ public class A {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("this is the first commit");
 		System.out.println("second commit");
 
 		System.out.println("***************");
